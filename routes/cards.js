c617e2b1d63900fad0e4df785ede3bef834bb0d0
Stephen Dawson <stephenjdawson@gmail.com>
2017-10-19 16:44:49 -0400

    refactoring, added functionality

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+//const data = require('../data/flashcardData.json').data;
+const { data } = require('../data/flashcardData.json');
+//const cards = data.cards;
+const { cards } = data;
 
 router.get('/', function(req, res){
-  res.render('card', {prompt: "Who is buried in Grants tomb?", hint: "Whose tomb is it?"});
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
+  const maxNumber = cards.length;
+  res.redirect("/cards/" + getRandomInt(0, maxNumber) + "?side=question");
+});
+
+router.get('/:id', function(req, res){
+  const name = req.cookies.username
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { side, id, text, hint, name };
+  // console.log(templateData);
+
+  res.render('card', templateData);
 });
 
 module.exports = router;
